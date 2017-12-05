b454328cb5d08df1d73940aa4b93754b32c939ac
Philippe GABERT <pgabert@philippes-mbp.apac.tibco.com>
2017-12-06 10:42:07 +1100

    ....

--- flogo-contrib/activity/configreader/activity.go
+++ flogo-contrib/activity/configreader/activity.go
@@ -14,22 +14,49 @@ var log = logger.GetLogger("activity-tibco-configreader")
 
 const (
 	configFile = "configFile"
+	readEachTime = "readEachTime"
 )
 
 type ConfigReader struct {
 	sync.Mutex
 	metadata *activity.Metadata
-	config map[string]string
+	gonfigConf Gonfig
 }
 
 // NewActivity creates a new activity
 func NewActivity(metadata *activity.Metadata) activity.Activity {
-	return &ConfigReader{metadata: metadata, config: make(map[string]string)}
+	return &ConfigReader{metadata: metadata}
 }
 
 // Metadata implements activity.Activity.Metadata
 func (a *ConfigReader) Metadata() *activity.Metadata {
 	return a.metadata
+}
+
+func (a *ConfigReader) getConfig(configFile string, reachEachTime bool, configName string, configType string) {
+	a.Lock()
+	defer a.Unlock()
+
+	if reachEachTime || a.gonfigConf == nil {
+		f, err := os.Open(configFile)
+		if err != nil {
+			log.Error("Error while opening file ! ", err)
+		}
+		defer f.Close();
+		a.gonfigConf, err := gonfig.FromJson(f)
+		if err != nil {
+	    	log.Error("Error while reading configuration file ! ", err)
+	    }
+	}
+
+    confValue, err := a.gonfigConf.GetString(configName, "default")
+	if err != nil {
+		log.Error("Error while getting configuration value ! ", err)
+	}
+
+	log.Info("Final value: ",confValue)
+
+	return confValue
 }
 
 // Eval implements activity.Activity.Eval
@@ -38,21 +65,12 @@ func (a *ConfigReader) Eval(context activity.Context) (done bool, err error)  {
 	configFile := context.GetInput(configFile).(string)
 	log.Debugf("Config [%s]", configFile)
 
-	f, err := os.Open(configFile)
-	if err != nil {
-		log.Error("Error while opening file ! ", err)
-	}
-	defer f.Close();
-	config, err := gonfig.FromJson(f)
-	if err != nil {
-    	log.Error("Error while reading configuration file ! ", err)
-    }
-
-    confValue, err := config.GetString("test_config", "default")
-	if err != nil {
-		log.Error("Error while getting configuration value ! ", err)
+	if context.GetInput(readEachTime) != nil {
+		readEachTimeB := context.GetInput(readEachTime).(bool)
 	}
 
-	log.Info("Final value: ",confValue)
+	configValue := a.getConfig(configFile, readEachTimeB, "test_config", "string")
+	log.Debugf("Final value returned [%s]", configValue)
+
 	return true, nil
 }
