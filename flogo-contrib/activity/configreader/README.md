1c6a76242a0ce4326748bc5a636a138b34ce07a2
Philippe GABERT <pgabert@philippes-mbp.apac.tibco.com>
2017-12-06 16:27:16 +1100

    Preparing for release !

--- flogo-contrib/activity/configreader/README.md
+++ flogo-contrib/activity/configreader/README.md
@@ -1,1 +1,61 @@
-# configreader
+# Config Reader
+This activity reads a JSON Configuration file. The configuration can then be used in following activities.
+This activity can read String, Int, Float and Boolean configuration values.
+Default value can be set
+Configuration can be cached (and not read at each execution) for better performances
+
+## Installation
+
+```bash
+flogo add activity github.com/philippegabert/configreader/flogo-contrib/activity/configreader
+```
+
+## Schema
+Inputs and Outputs:
+
+```json
+{
+  "inputs":[
+    {
+      "name": "configFile",
+      "type": "string",
+      "required": "true"
+    },
+    {
+      "name": "configName",
+      "type": "string",
+      "required": "true"
+    },
+    {
+      "name": "configType",
+      "type": "string",
+      "allowed" : ["string", "int", "float", "bool"]
+    },
+    {
+      "name": "defaultValue", 
+      "type": "any"
+    },
+    {
+      "name": "readEachTime",
+      "type": "bool"
+    }
+  ],
+  "outputs": [
+  	{
+      "name": "configValue",
+      "type": "any"
+    }
+  ]
+}
+```
+## Settings
+| Setting     | Description    |
+|:------------|:---------------|
+| configFile        | The path to the configuration file |         
+| configName        | The name of the configuration element to retrieve |
+| configType        | The type of the configuration element (string, int, float or bool) |
+| defaultValue        | The default value to set if the configuration was not read correctly |
+| readEachTime        | If set to true, the configuration file will be read at each execution of the flow. If set to false, the file will be read only once |
+| configValue        | The value of the configuration |
+
+## Configuration Examples
--- flogo-contrib/activity/configreader/activity_test.go
+++ flogo-contrib/activity/configreader/activity_test.go
@@ -1,10 +1,11 @@
 package configreader
 
 import (
+	"fmt"
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"io/ioutil"
 	"testing"
-	"github.com/TIBCOSoftware/flogo-lib/core/activity"
-	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
 )
 
 var activityMetadata *activity.Metadata
@@ -13,7 +14,7 @@ func getActivityMetadata() *activity.Metadata {
 
 	if activityMetadata == nil {
 		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
-		if err != nil{
+		if err != nil {
 			panic("No Json Metadata found for activity.json path")
 		}
 
@@ -34,7 +35,7 @@ func TestCreate(t *testing.T) {
 	}
 }
 
-func TestEval(t *testing.T) {
+func TestStringConfig(t *testing.T) {
 
 	defer func() {
 		if r := recover(); r != nil {
@@ -47,8 +48,93 @@ func TestEval(t *testing.T) {
 	tc := test.NewTestActivityContext(getActivityMetadata())
 
 	//setup attrs
+	tc.SetInput("configFile", "config.json")
+	tc.SetInput("configName", "string_config")
+	tc.SetInput("configType", "string")
+	tc.SetInput("readEachTime", "true")
 
 	act.Eval(tc)
 
 	//check result attr
+	val := tc.GetOutput("configValue")
+	fmt.Printf("String result : %v\n", val)
+
+}
+
+func TestIntConfig(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("configFile", "config.json")
+	tc.SetInput("configName", "int_config")
+	tc.SetInput("configType", "int")
+	tc.SetInput("readEachTime", "true")
+
+	act.Eval(tc)
+
+	//check result attr
+	val := tc.GetOutput("configValue")
+	fmt.Printf("Int result : %v\n", val)
+
+}
+
+func TestFloatConfig(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("configFile", "config.json")
+	tc.SetInput("configName", "float_config")
+	tc.SetInput("configType", "float")
+	tc.SetInput("readEachTime", "true")
+
+	act.Eval(tc)
+
+	//check result attr
+	val := tc.GetOutput("configValue")
+	fmt.Printf("Float result : %v\n", val)
+
+}
+
+func TestBoolConfig(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("configFile", "config.json")
+	tc.SetInput("configName", "bool_config")
+	tc.SetInput("configType", "bool")
+	tc.SetInput("readEachTime", "true")
+
+	act.Eval(tc)
+
+	//check result attr
+	val := tc.GetOutput("configValue")
+	fmt.Printf("Bool result : %v\n", val)
+
 }
